349216d683bd88318aadda9425668bb1d548a9bf
ankuraaryan5 <ankuraaryan5@gmail.com>
2024-09-22 20:09:17 +0530

    practice set completed

---
+++ codes/Abstract_36.java
@@ -0,0 +1,5 @@
+public class Abstract_36 {
+    public static void main(String[] args) {
+        
+    }
+}
--- codes/Practice_35.java
+++ codes/Practice_35.java
@@ -1,5 +1,8 @@
 class Circle{
     public int radius;
+    public Circle(){
+        System.out.println("I am just living");
+    }
     public Circle(int r){
         this.radius=r;
     }
@@ -8,19 +11,50 @@ class Circle{
     }
 }
 class Cylinder extends Circle{
-    int height;
+    private int height;
     public Cylinder(int r, int h){
         super(r);
         this.height=h;
-
     }
     public int volume(){
         return (int) (Math.PI*this.radius*this.radius*this.height);
     }
 }
+
+class Rectangle{
+    public int length;
+    public int breadth;
+    public int area;
+    public Rectangle(int l, int b){
+        this.length=l;
+        this.breadth=b;
+    }
+    public int getArea(){
+        return area = length*breadth;
+    }
+}
+class Cuboiod extends Rectangle{
+    private int height;
+    public Cuboiod(int l, int b,int h){
+        super(l, b);
+        this.height=h;
+
+    }
+    public int getVolume(){
+        // return (int) (getArea()*height);
+        return (int) (this.length*this.breadth*height);
+    }
+}
 public class Practice_35 {
     public static void main(String[] args) {
-        Cylinder cy = new Cylinder(3, 5);
-        System.out.println("volume of cylinder is "+cy.volume());
+        // Circle circle = new Circle(5);
+        // System.out.println("area of circle is: "+ circle.getArea());
+        // Cylinder cy = new Cylinder(3, 5);
+        // System.out.println("volume of cylinder is "+cy.volume());
+
+        Rectangle r = new Rectangle(3, 4);
+        System.out.println(r.getArea());
+        Cuboiod c = new Cuboiod(1, 3, 5);
+        System.out.println("volume of cuboid is:"+c.getVolume());
     }
 }
