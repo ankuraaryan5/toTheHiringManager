35831c0e9c495683f5e57933684434f691f7eddf
ankuraaryan5 <ankuraaryan5@gmail.com>
2024-09-30 22:28:18 +0530

    renamed file structure

--- Java/Threads_45.java
+++ Java/Threads_45.java
@@ -1,12 +1,13 @@
-class MyThread extends Thread{
+class MyThread extends Thread{ 
     public void run(){
         int i=0;
     while (i<5430) {
         
-        System.out.println("The thread is up and running");
+        System.out.println("The thread is up and running"); //--> stage3(running) and stage 4(non runnable i/o waiting)
         i++;
     }    
     }
+    // --> stage 5 (terminated)
 }
 
 class YourThread extends Thread{
@@ -37,9 +38,9 @@ class LatestThread implements Runnable{
 }
 public class Threads_45 {
     public static void main(String[] args) {
-        // MyThread m = new MyThread();
-        // // m.run();
-        // m.start();
+        // MyThread m = new MyThread(); --> stage 1(new)
+        // // m.run(); 
+        // m.start(); --> stage 2 (runnable)
         // YourThread y = new YourThread();
         // y.start();
         // on running this program we will see clearly what is happening 
