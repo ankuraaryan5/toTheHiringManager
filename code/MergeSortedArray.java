d63b0f50ee3aa93364205b979894d1e9d1873ddf
ankuraaryan5 <ankuraaryan5@gmail.com>
2024-10-09 22:58:11 +0530

    main

--- code/MergeSortedArray.java
+++ code/MergeSortedArray.java
@@ -1,5 +1,5 @@
 public class MergeSortedArray{
-    public static void Main(String[] Args){
+    public static void main(String[] Args){
         int [] arr={1,2,3,4};
         int [] brr ={2,4,6,8};
         
